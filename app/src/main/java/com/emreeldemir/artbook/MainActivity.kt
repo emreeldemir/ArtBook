0c7827ed17b0cf6c7c47946d569ba68256865a1b
Emre Eldemir <emreeldemir893@gmail.com>
2023-03-20 20:32:14 +0300

    options menu added

--- app/src/main/java/com/emreeldemir/artbook/MainActivity.kt
+++ app/src/main/java/com/emreeldemir/artbook/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.emreeldemir.artbook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.emreeldemir.artbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +16,24 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.art_menu, menu)
+        return super.onCreateOptionsMenu(menu)
 
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.add_art_item){
+            val intent = Intent(this@MainActivity, ArtActivity::class.java)
+            startActivity(intent)
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
