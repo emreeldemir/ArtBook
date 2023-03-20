1b7f9f688603e69b92a47b3689f0701e9807b10f
Emre Eldemir <emreeldemir893@gmail.com>
2023-03-20 19:50:57 +0300

    saveButton and selectImage funcs added

--- app/src/main/java/com/emreeldemir/artbook/ArtActivity.kt
+++ app/src/main/java/com/emreeldemir/artbook/ArtActivity.kt
@@ -2,10 +2,20 @@ package com.emreeldemir.artbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ArtActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_art)
+    }
+
+    fun saveButtonClicked (view : View){
+
+    }
+
+    fun selectImage (view : View) {
+
+
     }
 }
