f8aef172ef3755f0523ac873ce5acb863cfb7bc9
Emre Eldemir <emreeldemir893@gmail.com>
2023-03-20 19:36:04 +0300

    ArtActivity added

---
+++ app/src/main/java/com/emreeldemir/artbook/ArtActivity.kt
@@ -0,0 +1,11 @@
+package com.emreeldemir.artbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ArtActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_art)
+    }
+}
