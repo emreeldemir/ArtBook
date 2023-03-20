7b25f106d7ab95d7f0fce3387f94c002bdc83508
Emre Eldemir <emreeldemir893@gmail.com>
2023-03-20 21:07:00 +0300

    read external storage permission added as a draft

--- app/src/main/java/com/emreeldemir/artbook/ArtActivity.kt
+++ app/src/main/java/com/emreeldemir/artbook/ArtActivity.kt
@@ -1,9 +1,15 @@
 package com.emreeldemir.artbook
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.emreeldemir.artbook.databinding.ActivityArtBinding
+import com.google.android.material.snackbar.Snackbar
 
 class ArtActivity : AppCompatActivity() {
 
@@ -23,6 +29,30 @@ class ArtActivity : AppCompatActivity() {
 
     fun selectImage (view : View) {
 
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission", View.OnClickListener {
+
+
+                }).show()
+            } else {
+                // permission denied and don't ask again
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+            }
+        } else {
+            // permission granted
+            val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intentToGallery, 2)
+        }
 
     }
 }
+
+
+
+
+
+
+
+
